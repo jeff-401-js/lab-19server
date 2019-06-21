73e218e6bdb09caf68d874de0368d796978464d7
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-20 18:03:08 -0700

    server work

--- server.js
+++ server.js
@@ -3,3 +3,11 @@
 require('dotenv').config();
 
 const Q = require('@nmq/q/server');
+
+Q.start();
+
+const file = new Q('files');
+file.monitorEvent('save');
+file.monitorEvent('error');
+
+const db = new Q('database');
